952f218ca657e6a8e665c1f8443d4db42b7b4630
devbadalsingh <badal@only1loan.com>
2024-11-25 03:00:35 +0530

    Added the changesgn iwht pm2

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const PORT =  3000;
 
 app.get('/',(req,res)=>{
     return res.json({
-        message : `Hello their sucessfully worked on ci/cd`
+        message : `Hello their sucessfully worked on ci/cd with pm2`
     })
 })
 
 console.log("Hello ")
-console.log("NEw things added here")
+console.log("NEw things added here with the latest pm2 ")
 
 app.listen( PORT,()=>{
     console.log("The app is ")
