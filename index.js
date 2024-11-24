dd037d03f47c61da7f9d66fa56ccea86ea787c5f
devbadalsingh <badal@only1loan.com>
2024-11-25 02:46:20 +0530

    Addede the latest chaanges

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const PORT =  3000;
 
 app.get('/',(req,res)=>{
     return res.json({
-        message : `Hello their`
+        message : `Hello their sucessfully worked on ci/cd`
     })
 })
 
 console.log("Hello ")
+console.log("NEw things added here")
 
 app.listen( PORT,()=>{
     console.log("The app is ")
